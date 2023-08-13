fbaaafa0c66e75fb1e0d60444ba85ba8f4ba77d5
acer <acer@ASUR>
2023-08-13 21:30:53 +0530

    Add project files.

---
+++ CountEvenOdd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountEvenOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int evenCount = 0;
+            int oddCount = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine("Even elements: " + evenCount);
+            Console.WriteLine("Odd elements: " + oddCount);
+            Console.ReadLine();
+        }
+    }
+}
